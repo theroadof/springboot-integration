ee581527e711a131b2cf5b24da00e998f0951ea9
Devin <991476256@qq.com>
2020-07-29 15:35:57 +0800

    [employee][devin hardy] test: add should_return_companies_when_getCompanies_given_ test for CompanyService

---
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+    public List<Company> getCompanies() {
+        return null;
+    }
+}
---
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CompanyServiceTest {
+
+    @Autowired
+    CompanyService companyService;
+
+    @Test
+    void should_return_companies_when_getCompanies_given_() {
+        //given
+
+        //when
+        List<Company> companies = companyService.getCompanies();
+
+        //then
+        assertNotNull(companies);
+
+    }
+}
