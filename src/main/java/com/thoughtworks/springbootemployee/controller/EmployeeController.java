bef7ada8e8fdd1245b52dce6ba43bf449e3cccbe
Devin <991476256@qq.com>
2020-07-29 15:27:29 +0800

    [employee][devin hardy] refactor: refactor EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,20 +1,20 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
-import com.thoughtworks.springbootemployee.utils.PageUtils;
-import org.springframework.beans.BeanUtils;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
 
     private static final String ADD_EMPLOYEE_SUCCESS = "add employee success";
     private static final String UPDATE_EMPLOYEE_SUCCESS = "update employee success";
@@ -25,47 +25,34 @@ public class EmployeeController {
                                        @RequestParam(value = "pageSize", required = false) Integer pageSize,
                                        @RequestParam(value = "gender", required = false) String gender) {
         if (Objects.nonNull(gender)) {
-            return getEmployeeList().stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
+            return employeeService.queryEmployeesByGender(gender);
         }
         if (Objects.nonNull(page) && Objects.nonNull(pageSize)) {
-            return new PageUtils<Employee>().getPage(getEmployeeList(), page, pageSize);
+            return employeeService.queryEmployeesByPage(page, pageSize);
         }
-        return getEmployeeList();
+        return employeeService.queryEmployees();
     }
 
     @GetMapping("/{id}")
     public Employee getEmployee(@PathVariable("id") int id) {
-        List<Employee> employees = getEmployeeList();
-        return employees.stream().filter(employee -> employee.getId() == id).findFirst().orElse(null);
+        return employeeService.queryEmployee(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String addEmployee(@RequestBody Employee employee) {
-        List<Employee> employees = getEmployeeList();
-        if (!employees.contains(employee)) {
-            employees.add(employee);
-        }
-        return ADD_EMPLOYEE_SUCCESS;
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
     }
 
     @PutMapping("/{id}")
-    public String updateEmployee(@PathVariable("id") String id, @RequestBody Employee employee) {
-        List<Employee> employees = getEmployeeList();
-        //todo
-        return UPDATE_EMPLOYEE_SUCCESS;
+    public Employee updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(employee);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteEmployee(@PathVariable("id") String id) {
-        //// TODO: 7/29/2020
+    public String deleteEmployee(@PathVariable("id") int id) {
+        employeeService.deleteEmployee(id);
         return DELETE_EMPLOYEE_SUCCESS;
     }
 
-    private List<Employee> getEmployeeList() {
-        List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1, "q", 18, "male", new BigDecimal(3000)));
-        employees.add(new Employee(2, "w", 18, "male", new BigDecimal(3000)));
-        return employees;
-    }
 }
