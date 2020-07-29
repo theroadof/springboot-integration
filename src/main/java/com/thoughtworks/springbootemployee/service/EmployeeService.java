3a993f78f250b1d101c365c04ebf33e23f21804f
Devin <991476256@qq.com>
2020-07-29 13:05:13 +0800

    [employee][devin] test: add should_return_employees_when_getEmployees_given_ test

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,9 +1,20 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
 
 
+    public EmployeeService(EmployeeRepository employeeRepository) {
+
+    }
+
+    public List<Employee> queryEmployees() {
+        return null;
+    }
 }
--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -1,5 +1,28 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
 public class EmployeeServiceTest {
 
+    @Test
+    void should_return_employees_when_getEmployees_given_() {
+        //given
+        EmployeeService employService = new EmployeeService(new EmployeeRepository());
+
+        //when
+        List<Employee> employees = employService.queryEmployees();
+
+        //then
+        assertNotNull(employees);
+
+    }
 }
