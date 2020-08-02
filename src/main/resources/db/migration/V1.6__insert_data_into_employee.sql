6360ef40b44d9c241037e7bb6c1b7d7b46b5a10e
theroadof <1294911641@qq.com>
2020-08-02 11:53:25 +0800

    [flyway][Teddy]feature:insert data into employee

---
+++ src/main/resources/db/migration/V1.6__insert_data_into_employee.sql
@@ -0,0 +1,7 @@
+insert into employee values (1,'Teddy',22,'male',666,1);
+insert into employee values (2,'Zero',22,'male',666,1);
+insert into employee values (3,'Bruno',22,'male',666,1);
+insert into employee values (4,'Austin',22,'male',666,1);
+insert into employee values (5,'Jessie',22,'female',666,1);
+insert into employee values (6,'Steve',22,'male',666,1);
+
